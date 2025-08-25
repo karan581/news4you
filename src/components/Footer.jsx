c77632c7340f951178a7fe6d132c6efac5b1a4f7
Karan Debnath <33799147+karan581@users.noreply.github.com>
2025-08-25 13:56:14 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,11 @@ export default function Footer() {
     return (
         <footer>
             <div className="container">
-                <p>&copy; 2025 News 4 You Daily</p>
+                <p>&copy; 2025 | News 4 You Daily | Website by Karan Debnath</p>
             </div>
         </footer>
     );
 
 }
+
 
